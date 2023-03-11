ec7084d3c585b835d5d3310ffdb7e4319db2da96
Joel S.Butron <samirbut@hotmail.com>
2023-03-11 16:35:56 -0600

    Ejercicio Lasagna completado

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,21 @@
 public class Lasagna {
+    int expectedTimeInOven = 40;
     // TODO: define the 'expectedMinutesInOven()' method
+    public int expectedMinutesInOven() {
+        return expectedTimeInOven;
+    }
 
     // TODO: define the 'remainingMinutesInOven()' method
+    public int remainingMinutesInOven(int minutesInOven){
+        return expectedTimeInOven-minutesInOven;
+    }
 
     // TODO: define the 'preparationTimeInMinutes()' method
-
+    public int preparationTimeInMinutes(int layers) {
+        return layers*2;
+    }
     // TODO: define the 'totalTimeInMinutes()' method
+    public int totalTimeInMinutes(int layers,int minutesInOven) {
+        return (layers*2)+(minutesInOven);
+    }
 }
